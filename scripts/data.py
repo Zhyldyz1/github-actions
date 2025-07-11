6afabb15cf9fe2fce3662b3eb480ae64f8de3727
Zhyldyz1 <zh.toktorova@gmail.com>
2025-07-10 21:00:14 -0500

    Adding session4 Python

--- scripts/data.py
+++ scripts/data.py
@@ -1,19 +1,7 @@
 import json
-import os
-
-output_directory = "scripts"
-
-
-data = {"key": "value", "status": "processed"}
-
-
-if not os.path.exists(output_directory):
-    os.makedirs(output_directory)  # Creates the directory recursively if intermediate directories are missing
-
-output_file = os.path.join(output_directory, "python-data.json")
-
-
-with open(output_file, 'w') as f: 
-    json.dump(data, f, indent=4)  
-
-print(f"JSON file '{output_file}' created successfully.")
+data = {
+    "key": "value",
+    "status": "processed"
+}
+with open("scripts/python-data.json", "w") as f:
+    json.dump(data, f)
