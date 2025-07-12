657d08cad6cb260dc8e83e1a0e742ac3f97f004f
Zhyldyz1 <zh.toktorova@gmail.com>
2025-07-12 17:15:45 -0500

    infra-s3

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "github-actions-tf-task-zhyldyz"
-    key    = "github-actions/s3-task/terraform.tfstate"
+    key    = "github-actions/infra/terraform.tfstate"
     region = "us-east-1"
   }
 }
