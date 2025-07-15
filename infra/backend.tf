0d3ef000527ac78825631870e7bc793854ecc363
Zhyldyz1 <zh.toktorova@gmail.com>
2025-07-14 21:35:10 -0500

    infra-s3

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-session-backend-bucket-zhyldyz"
-    key    = "session-9/terraform.tfstate"
+    key    = "session-6/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
   }
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,8 @@ variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
 }
+
+variable "aws_region" {
+  description = "The AWS region where the S3 bucket is located"
+  type        = string
+}
